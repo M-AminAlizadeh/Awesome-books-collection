960088594c61bbcca5ff5797d7fc3318ae6bd441
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-05-03 17:22:59 +0330

    Show the time using luxon

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { listOfBooks, removeBook } from './modules/add-remove-book.js';
 import { spa } from './modules/spa.js';
+import { timeContainer,DateTime } from "./modules/luxon.js";
 
 // Add a book
 listOfBooks;
@@ -9,3 +10,6 @@ removeBook();
 
 // SPA nav
 spa();
+
+// Luxon
+timeContainer.innerHTML = DateTime.now();

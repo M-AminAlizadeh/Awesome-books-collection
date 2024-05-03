2cd5b67a3e9463676ce5add435b345febff9967c
m-aminalizadeh <aminalizadeh@riseup.net>
2024-05-03 08:47:58 +0330

    Update index.js and add router to it

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import currentYear from './modules/date.js';
+import { openMenu, closeMenu } from './modules/menuToggler.js';
+import router from './modules/router.js';
+
+router();
+currentYear();
+openMenu();
+closeMenu();

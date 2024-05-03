a8ba39fa1d430fcc61e35888699c8b9ab49b453e
m-aminalizadeh <aminalizadeh@riseup.net>
2024-05-03 09:19:41 +0330

    Complete the structure of contactme page

--- src/modules/pages/contactMePage.js
+++ src/modules/pages/contactMePage.js
@@ -1,6 +1,31 @@
-const contactMePage = () => {
-  console.log('contactMePage');
-  return 'contactMePage';
-};
+const contactMePage = () => (
+  `
+    <section class="p-5 d-flex flex-column flex-wrap justify-content-center align-items-start">
+      <h2>Contact me</h2>
+        <p class="fs-6">
+          Do you have any questions or do you just want to say Hello?<br>
+          You can reach out to me!
+          </p>
+        <ul class="list-unstyled"> 
+          <li>
+            <a href="https://www.linkedin.com/in/m-amin-alizadeh" class="d-flex align-items-center text-decoration-none">
+              <img src="https://img.icons8.com/color/30/linkedin.png" alt="LinkedIn"/>
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/M-AminAlizadeh" class="d-flex align-items-center text-decoration-none">
+              <img src="https://img.icons8.com/sf-regular/30/github.png" alt="GitHub"/>
+              GitHub
+            </a>
+          </li>
+        </ul>
+        <p>
+          If you find any issues in this project you can make an issue 
+          <a href="https://github.com/M-AminAlizadeh/Awesome-books-collection/issues">Here</a>
+        </p>
+    </section>
+  `
+);
 
 export default contactMePage;

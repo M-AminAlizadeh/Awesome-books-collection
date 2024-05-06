a53e250b71cf980836c8a94c73230cb83d6c28a2
m-aminalizadeh <aminalizadeh@riseup.net>
2024-05-06 18:17:28 +0330

    Add render books feature

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import currentYear from './modules/date.js';
 import { openMenu, closeMenu } from './modules/menuToggler.js';
 import router from './modules/router.js';
 import addBook from './modules/addBook.js';
+import renderBooks from './modules/renderBooks.js';
 
 router();
 currentYear();
 openMenu();
 closeMenu();
 addBook();
+renderBooks();

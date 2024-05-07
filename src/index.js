b2418b741be8ed3dcbae1a9300e77ba4ad818abd
m-aminalizadeh <aminalizadeh@riseup.net>
2024-05-07 13:48:36 +0330

    Add date and time

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import currentYear from './modules/date.js';
+import date from './modules/date.js';
 import { openMenu, closeMenu } from './modules/menuToggler.js';
 import router from './modules/router.js';
 import addBook from './modules/addBook.js';
 import renderBooks from './modules/renderBooks.js';
 import removeBook from './modules/removeBook.js';
 import editBook from './modules/editBook.js';
+import time from './modules/time.js';
 
 router();
-currentYear();
+date();
+time();
 openMenu();
 closeMenu();
 addBook();
